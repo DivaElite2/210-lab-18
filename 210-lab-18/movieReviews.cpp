d8c470616a42f929385c7fdfb015203ba36068d6
Lateefah camacho <lateefah.camacho07@gmail.com>
2025-10-03 21:07:48 -0700

    while loop

--- 210-lab-18/movieReviews.cpp
+++ 210-lab-18/movieReviews.cpp
@@ -21,7 +21,52 @@ int main()
     ReviewNode* tail = nullptr;
     int methodChoice;
     char continueChoice;
-    int reviewCount
+    int reviewCount = 0;
+    double totalRating = 0.0;
+
+    //Get the insertion from user
+cout << "Which linked list method should we use?" << endl;
+    cout << "    [1] New nodes are added at the head of the linked list" << endl;
+    cout << "    [2] New nodes are added at the tail of the linked list" << endl;
+    cout << "    Choice: ";
+    cin >> methodChoice;
+    cin.ignore();
+
+    // start input loop
+       char choice = 'y';
+    while (choice == 'y' || choice == 'Y') {
+        // Get review data
+        double rating;
+        cout << "Enter review rating 0-5: ";
+        cin >> rating;
+        cin.ignore();
+        
+        string comment;
+        cout << "Enter review comments: ";
+        getline(cin, comment);
+        
+        // Create new node
+        ReviewNode* newNode = new ReviewNode{rating, comment, nullptr};
+        
+        // Add to list
+        if (head == nullptr) { // First node
+            head = tail = newNode;
+        } else if (methodChoice == 1) { // Add at head
+            newNode->next = head;
+            head = newNode;
+        } else { // Add at tail
+            tail->next = newNode;
+            tail = newNode;
+        }
+        
+        reviewCount++;
+        totalRating += rating;
+        
+        // Continue?
+        cout << "Enter another review? Y/N: ";
+        cin >> choice;
+        cin.ignore();
+    }
 
 
     return 0;
