8c84dd432d5ebddaa26d2db4edd14e402ca7b5b7
Lateefah camacho <lateefah.camacho07@gmail.com>
2025-10-03 20:57:16 -0700

    initial

---
+++ 210-lab-18/movieReviews.cpp
@@ -0,0 +1,30 @@
+//Lateefah Camacho
+//210-lab-18
+//vscode
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// struct for the linked list
+struct ReviewNode{
+    double rating ;
+    string comment;
+    ReviewNode* next;
+
+};
+
+int main()
+{
+    ReviewNode* head = nullptr;
+    ReviewNode* tail = nullptr;
+    int methodChoice;
+    char continueChoice;
+    int reviewCount
+
+
+    return 0;
+}
+
+
